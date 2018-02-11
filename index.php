a6408a1e1bf68e83a1e784e4b480901899537563
Ichsan Maulana <michsanm1999@gmail.com>
2018-02-11 20:22:07 +0700

    Replace Index.php

--- index.php
+++ index.php
@@ -1,55 +1,60 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<?php
 
-        <title>Ichsan Maulana</title>
+/**
+ * Laravel - A PHP Framework For Web Artisans
+ *
+ * @package  Laravel
+ * @author   Taylor Otwell <taylor@laravel.com>
+ */
 
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-        <link rel="stylesheet" type="text/css" href="/public/css/custom.css">
-        <link rel="stylesheet" type="text/css" href="/public/css/jquery.fullpage.min.css">
-        <!-- Styles -->    
-    </head>
-    <body>
-        <div id="fullpage">
-            <div class="section " id="section0">
-                <div class="flex-center position-ref full-height">
-                    <div class="content">
-                        <div class="title m-b-md f-col-white">
-                            Nice To Meet You!
-                        </div>
-                        <div class="title-sub m-b-md f-col-white">                  
-                            please come back when i'ts ready
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="section " id="section1">
-                <div class="flex-center position-ref full-height">
-                    <div class="content">
-                        <div class="f-col-white">
-                            <p>Made with <i class="fa fa-heart f-col-red"></i> by Ichsan Maulana</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </body>
+define('LARAVEL_START', microtime(true));
 
-    <script type="text/javascript" src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
-    <script type="text/javascript" src="/public/js/jquery.fullpage.min.js"></script>
-    <script type="text/javascript">
-        $(document).ready(function() {
-            $('#fullpage').fullpage({
-              sectionsColor: ['#000000','#000000'],
-              anchors: ['Intro', 'Credit'],
-              menu: '#menu',
-            });
-        });
-    </script>
+/*
+|--------------------------------------------------------------------------
+| Register The Auto Loader
+|--------------------------------------------------------------------------
+|
+| Composer provides a convenient, automatically generated class loader for
+| our application. We just need to utilize it! We'll simply require it
+| into the script here so that we don't have to worry about manual
+| loading any of our classes later on. It feels great to relax.
+|
+*/
 
-</html>
+require __DIR__.'/../vendor/autoload.php';
+
+/*
+|--------------------------------------------------------------------------
+| Turn On The Lights
+|--------------------------------------------------------------------------
+|
+| We need to illuminate PHP development, so let us turn on the lights.
+| This bootstraps the framework and gets it ready for use, then it
+| will load up this application so that we can run it and send
+| the responses back to the browser and delight our users.
+|
+*/
+
+$app = require_once __DIR__.'/../bootstrap/app.php';
+
+/*
+|--------------------------------------------------------------------------
+| Run The Application
+|--------------------------------------------------------------------------
+|
+| Once we have the application, we can handle the incoming request
+| through the kernel, and send the associated response back to
+| the client's browser allowing them to enjoy the creative
+| and wonderful application we have prepared for them.
+|
+*/
+
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
+
+$response->send();
+
+$kernel->terminate($request, $response);
